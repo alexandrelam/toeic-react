4e3709cd7536e5e874c79453c98a944de5acf956
alexandrelam <alexandrelam@outlook.com>
2020-11-01 21:52:20 +0100

    fix 4 ? 5 name incorrect donc j'ai changé le nom dans les settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   pathPrefix: `/toeic`,
   siteMetadata: {
-    title: `TOEIC !`,
+    title: `TOEIC`,
     description: `Enregistre tes réponses de tes entraînements au toeic !`,
     author: `@alexandrelam`,
   },
